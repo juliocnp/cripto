5693dc7e81d98c3e8147f384bdd6bfb3d116336e
Júlio César <julioccnp@gmail.com>
2019-10-16 19:49:45 -0300

    é isso aí

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TelaComponent } from './tela/tela.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TelaComponent } from './tela/tela.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/tela/tela.component.ts
+++ src/app/tela/tela.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela.component.scss']
 })
 export class TelaComponent implements OnInit {
+  Nome = '';
+  CPF = '';
+  Idade = '';
+  Endereco = '';
+  Sexo = '';
 
+  NomeCriptografado = '';
+  CPFCriptografado = '';
+  IdadeCriptografado = '';
+  EnderecoCriptografado = '';
+  SexoCriptografado = '';
+
+  NomeNormal = '';
+  CPFNormal = '';
+  IdadeNormal = '';
+  EnderecoNormal = '';
+  SexoNormal = '';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  copyObject(object: any) {
+    return JSON.parse(JSON.stringify(object));
   }
 
 }
